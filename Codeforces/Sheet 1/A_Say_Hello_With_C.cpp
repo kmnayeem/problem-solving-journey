998ba6e811a1652410fbbe1c262e11d5455c40f8
K. M. Abdullah Al - Nayeem <kmabdullah2025@gmail.com>
2026-05-28 02:57:34 +0600

    Add files via upload

---
+++ Codeforces/Sheet 1/A_Say_Hello_With_C.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+//Include all standerd C++ libraries
+using namespace std;
+//Allow us to use cin, cout without writing std::
+int main()
+//Main function where program execution start
+{
+    string s;
+    //A variable "s" is created to store text(string)
+    cin >> s;
+    //Takes input from user and store it in "s"
+    cout << "Hello, " << s;
+    //print "Hello, and your input text" on the screen
+    return 0;
+    //Include successful execution of the program
+}
