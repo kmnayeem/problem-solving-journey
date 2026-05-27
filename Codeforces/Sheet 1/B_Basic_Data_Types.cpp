053de5331c6111ab83252468372c633033a66c35
K. M. Abdullah Al - Nayeem <kmabdullah2025@gmail.com>
2026-05-28 02:57:54 +0600

    Add files via upload

---
+++ Codeforces/Sheet 1/B_Basic_Data_Types.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+// Includes all standard C++ libraries
+using namespace std;
+//Allow us to use cin, cout without writing std::
+int main() 
+{
+    // Variable declarations with different data types
+    int i;               // Integer type
+    long long int ll;    // Large integer type
+    char ch;             // Character type
+    float f;             // Floating point number
+    double d;            // Double precision floating point number
+
+    // Taking input from the user
+    cin >> i >> ll >> ch >> f >> d;
+    // Printing each value on a new line
+    cout << i << endl;
+    cout << ll << endl;
+    cout << ch << endl;
+    cout << f << endl;
+    cout << d << endl;
+    return 0;
+}
