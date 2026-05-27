e3cd0ce732bbb9892b527b16dfe04b08dd3119a7
K. M. Abdullah Al - Nayeem <kmabdullah2025@gmail.com>
2026-05-28 02:58:17 +0600

    Add files via upload

---
+++ Codeforces/Sheet 1/C_Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    long long int x,y;
+    cin >> x >> y;
+    // Printing the sum of x and y
+    cout << x << " + " << y << " = " << x + y << endl;
+    // Printing the multiplication of x and y
+    cout << x << " * " << y << " = " << x * y << endl;
+    // Printing the subtraction of x and y
+    cout << x << " - " << y << " = " << x - y << endl;
+    return 0;
+}
