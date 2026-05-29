0a9a600092377a29b76f3f5968b23c8b23644f8c
K. M. Abdullah Al Nayeem <kmabdullah2025@gmail.com>
2026-05-29 23:38:19 +0600

    Add files via upload

---
+++ Codeforces/Sheet 1/P_First_digit.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    int firstDigit = x / 1000;
+    // condition ? value_if_true : value_if_false
+    cout << (firstDigit % 2 == 0 ? "EVEN" : "ODD");
+    return 0;
+}
+
+/*
+========================================================
+Topic   : Check First Digit Parity
+Author  : kmnayeem
+========================================================
+
+1. Input Constraint (Codeforces):
+    - 1000 < x ≤ 9999
+    - So x is always a 4-digit number.
+
+2. Why divide by 1000:
+    - A 4-digit number ABCD can be written as:
+        ABCD / 1000 = A
+        1234 / 1000 = 1
+    - This extracts the first digit.
+
+========================================================
+*/
