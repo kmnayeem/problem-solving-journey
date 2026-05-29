3dd2a1c2f3524f77f3facda8bc5ee2dca28dd915
K. M. Abdullah Al Nayeem <kmabdullah2025@gmail.com>
2026-05-30 02:08:22 +0600

    Update F_Digits_Summation.cpp

--- Codeforces/Sheet 1/F_Digits_Summation.cpp
+++ Codeforces/Sheet 1/F_Digits_Summation.cpp
@@ -1,19 +1,33 @@
+/*
+    ========================================================
+    Author  : kmnayeem
+    Topic   : Sum of Last Digits
+    Problem : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+    ========================================================
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() 
 {
-    long long int n, m;
+    long long int n, m; 
     cin >> n >> m;
+
+    // Extracting last digit of each number using modulo 10
     int last_digit_n = n % 10;
     int last_digit_m = m % 10;
+    
     cout << last_digit_n + last_digit_m;
+
     return 0;
 }
-    /* 
-    Modulo 10 returns the remainder after division by 10,
-    which is always the last digit of the number.
-    Example:
-    13 % 10 = 3
-    127 % 10 = 7
-    */
 
+/*
+    Modulo 10 returns the remainder after division by 10,
+    which represents the last digit of a number.
+
+    Example:
+        13 % 10  = 3
+        127 % 10 = 7
+*/
