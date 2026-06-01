93c8d14ef8217fe649a332f941b9f023d72b9a9d
K. M. Abdullah Al Nayeem <kmabdullah2025@gmail.com>
2026-06-02 00:19:59 +0600

    Update 04_Inverted_Right_Half_Pyramid.cpp

--- Patterns/Star_pattern/04_Inverted_Right_Half_Pyramid.cpp
+++ Patterns/Star_pattern/04_Inverted_Right_Half_Pyramid.cpp
@@ -14,11 +14,11 @@
         *
 
     Pattern Explanation:
-    Row 1 -> 5 stars + 0 spaces
-    Row 2 -> 4 stars + 1 space
-    Row 3 -> 3 stars + 2 spaces
-    Row 4 -> 2 stars + 3 spaces
-    Row 5 -> 1 star  + 4 spaces
+    Row 1 -> 0 space + 5 star
+    Row 2 -> 1 space + 4 star
+    Row 3 -> 2 space + 3 star
+    Row 4 -> 3 space + 2 star
+    Row 5 -> 4 space + 1 star
 */
 
 #include<bits/stdc++.h>
