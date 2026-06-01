fe187030b907539ad1232a2ad2181976fcce6800
K. M. Abdullah Al Nayeem <kmabdullah2025@gmail.com>
2026-06-02 00:09:50 +0600

    Add files via upload

---
+++ Patterns/Star_pattern/03_ Right_Half_Pyramid.cpp
@@ -0,0 +1,77 @@
+/*
+    Author : kmnayeem
+    Description :
+    This program prints a Right Half Pyramid star (*) pattern.
+
+    Example Input:
+    5
+
+    Example Output:
+        *
+       **
+      ***
+     ****
+    *****
+
+    Pattern Explanation:
+    Row 1 -> 4 spaces + 1 star
+    Row 2 -> 3 spaces + 2 stars
+    Row 3 -> 2 spaces + 3 stars
+    Row 4 -> 1 space  + 4 stars
+    Row 5 -> 0 spaces + 5 stars
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int star = 1; // stars in current row
+    int space = n - 1; // leading spaces
+    for(int i=1; i<=n; i++)
+    {
+        // print spaces
+        for(int j=1; j<=space; j++)
+        {
+            cout << " ";
+        }
+        // print stars
+        for(int j=1; j<=star; j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+        star++; // next row has one more star
+        space--; // next row has one less space
+    }
+    return 0;
+}
+
+/*
+==================== OPTIMIZED VERSION ====================
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n-i; j++)
+        {
+            cout << " ";
+        }
+        for(int j=1; j<=i; j++)
+        {
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+
+===========================================================
+*/
